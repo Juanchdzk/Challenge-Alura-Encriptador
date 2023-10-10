5abf849bbd2ec8f981831417c11962aedbaa9f42
Juanchdzk <97260812+Juanchdzk@users.noreply.github.com>
2023-10-10 15:26:56 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ function btnCopiar(){
     outputDesencriptado.select();
     navigator.clipboard.writeText(outputDesencriptado.value);
     outputDesencriptado.value = "";
-    alert("Texto copiado");
 }
 
 /*
@@ -34,24 +33,33 @@ Encriptador y desencriptador
 */ 
 
 function encriptarTexto(textoEncriptado){
-    textoEncriptado = textoEncriptado.toLowerCase();
+    if(textoEncriptado.length != 0){
+        textoEncriptado = textoEncriptado.toLowerCase();
 
-    for(var i = 0; i < matrizVocales.length; i++){
-        if (textoEncriptado.includes(matrizVocales[i][0])){
-            textoEncriptado = textoEncriptado.replaceAll(matrizVocales[i][0],matrizVocales[i][1]);
+        for(var i = 0; i < matrizVocales.length; i++){
+            if (textoEncriptado.includes(matrizVocales[i][0])){
+                textoEncriptado = textoEncriptado.replaceAll(matrizVocales[i][0],matrizVocales[i][1]);
+            }
         }
+            return textoEncriptado;
+    } else {
+        alert("No hay mensaje que encriptar");
+        console.log(inputEncriptado[0].value);
     }
-    return textoEncriptado;
 }
 
-
 function desencriptarTexto(textoDesencriptado){
-    textoDesencriptado = textoDesencriptado.toLowerCase();
+    if(textoDesencriptado.length != 0){
+        textoDesencriptado = textoDesencriptado.toLowerCase();
 
-    for(var i = 0; i < matrizVocales.length; i++){
-        if (textoDesencriptado.includes(matrizVocales[i][1])){
-            textoDesencriptado = textoDesencriptado.replaceAll(matrizVocales[i][1],matrizVocales[i][0]);
+        for(var i = 0; i < matrizVocales.length; i++){
+            if (textoDesencriptado.includes(matrizVocales[i][1])){
+                textoDesencriptado = textoDesencriptado.replaceAll(matrizVocales[i][1],matrizVocales[i][0]);
+            }
         }
+        return textoDesencriptado;
+    } else {
+        alert("No hay mensaje que desencriptar");
+        console.log(outputDesencriptado[0].value);
     }
-    return textoDesencriptado;
 }
